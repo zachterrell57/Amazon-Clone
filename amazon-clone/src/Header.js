5a23370586c468156192fd468c1e1ec58a2cac8c
Zachary Terrell <zachterrell@gmail.com>
2021-08-23 15:01:39 -0500

    added links

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search"
+
 function Header() {
   return (
     <nav className="header">
@@ -10,9 +12,35 @@ function Header() {
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
           alt="amazon-logo"
         ></img>
-
-        <input type="text" className="header_searchInput" />
       </Link>
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon"/>
+      </div>
+      
+      <div className="header_nav">
+        {/* 1st link */}
+        <Link to="/login" className="header_link">
+          <div className="header_option">
+              <span className="header_optionLineOne">Hello</span>
+              <span className="header_optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        {/* 2nd link */}
+        <Link to="/" className="header_link">
+          <div className="header_option">
+              <span className="header_optionLineOne">Returns</span>
+              <span className="header_optionLineTwo">And Orders</span>              
+          </div>
+        </Link>
+        {/* 3rd link */}
+        <Link to="/" className="header_link">
+          <div className="header_option">
+              <span className="header_optionLineOne">Your</span>
+              <span className="header_optionLineTwo">Prime</span>              
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
