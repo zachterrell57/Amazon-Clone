f6a80b0cf0e821a34b8728ca2f1c7c8dc88e4785
Zachary Terrell <zachterrell@gmail.com>
2021-08-28 22:38:29 -0500

    header done

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search"
+import { ShoppingBasket } from "@material-ui/icons";
 
 function Header() {
   return (
     <nav className="header">
-      <Link to="/login">
+      <Link to="/">
         <img
           className="header_logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
@@ -19,25 +20,28 @@ function Header() {
       </div>
       
       <div className="header_nav">
-        {/* 1st link */}
         <Link to="/login" className="header_link">
           <div className="header_option">
               <span className="header_optionLineOne">Hello</span>
               <span className="header_optionLineTwo">Sign In</span>
           </div>
         </Link>
-        {/* 2nd link */}
         <Link to="/" className="header_link">
           <div className="header_option">
               <span className="header_optionLineOne">Returns</span>
               <span className="header_optionLineTwo">And Orders</span>              
           </div>
         </Link>
-        {/* 3rd link */}
         <Link to="/" className="header_link">
           <div className="header_option">
               <span className="header_optionLineOne">Your</span>
               <span className="header_optionLineTwo">Prime</span>              
+          </div>
+        </Link>
+        <Link to="/checkout" className="header_link">
+          <div className="header_optionBasket">
+            <ShoppingBasket></ShoppingBasket>
+            <span className="header_optionLineTwo header_basketCount">0</span>
           </div>
         </Link>
       </div>
